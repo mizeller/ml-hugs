fee9c698b81aa0dd22c2d23e7d56491b03028a76
michel zeller <mizeller@ethz.ch>
2024-05-03 15:53:06 +0200

    update graphics utils w/ latest version from 3DGs (used in GOF)

--- hugs/utils/graphics_utils.py
+++ hugs/utils/graphics_utils.py
@@ -1,6 +1,3 @@
-# Code based on 3DGS: https://github.com/graphdeco-inria/gaussian-splatting/blob/main/utils/graphics_utils.py
-# License from 3DGS: https://github.com/graphdeco-inria/gaussian-splatting/blob/main/LICENSE.md
-
 #
 # Copyright (C) 2023, Inria
 # GRAPHDECO research group, https://team.inria.fr/graphdeco
@@ -12,19 +9,15 @@
 # For inquiries contact  george.drettakis@inria.fr
 #
 
-import cv2
 import torch
 import math
 import numpy as np
 from typing import NamedTuple
 
-
 class BasicPointCloud(NamedTuple):
     points : np.array
     colors : np.array
     normals : np.array
-    faces : None
-
 
 def geom_transform_points(points, transf_matrix):
     P, _ = points.shape
@@ -35,14 +28,12 @@ def geom_transform_points(points, transf_matrix):
     denom = points_out[..., 3:] + 0.0000001
     return (points_out[..., :3] / denom).squeeze(dim=0)
 
-
 def getWorld2View(R, t):
     Rt = np.zeros((4, 4))
     Rt[:3, :3] = R.transpose()
     Rt[:3, 3] = t
     Rt[3, 3] = 1.0
     return np.float32(Rt)
-
 
 def getWorld2View2(R, t, translate=np.array([.0, .0, .0]), scale=1.0):
     Rt = np.zeros((4, 4))
@@ -57,23 +48,7 @@ def getWorld2View2(R, t, translate=np.array([.0, .0, .0]), scale=1.0):
     Rt = np.linalg.inv(C2W)
     return np.float32(Rt)
 
-
-def get_projection_matrix_center(znear, zfar, fx, fy, cx, cy, width, height):
-    P = torch.zeros(4, 4)
-    cx = width - cx
-    z_sign = 1.0
-    P[0, 0] = 2.0 * fx / width
-    P[1, 1] = 2.0 * fy / height
-    P[0, 2] = 1.0 - 2.0 * cx / width
-    P[1, 2] = 2.0 * cy / height - 1.0
-    P[3, 2] =  z_sign
-
-    P[2, 2] = z_sign * zfar / (zfar - znear)
-    P[2, 3] = -(zfar * znear) / (zfar - znear)
-    return P
-
-
-def get_projection_matrix(znear, zfar, fovX, fovY):
+def getProjectionMatrix(znear, zfar, fovX, fovY):
     tanHalfFovY = math.tan((fovY / 2))
     tanHalfFovX = math.tan((fovX / 2))
 
@@ -95,10 +70,8 @@ def get_projection_matrix(znear, zfar, fovX, fovY):
     P[2, 3] = -(zfar * znear) / (zfar - znear)
     return P
 
-
 def fov2focal(fov, pixels):
     return pixels / (2 * math.tan(fov / 2))
-
 
 def focal2fov(focal, pixels):
     return 2*math.atan(pixels/(2*focal))
